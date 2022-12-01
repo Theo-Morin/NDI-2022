3ec9880422494ebda3c608ee4e2cf937e3442a41
Théo MORIN <theo@hetchr.com>
2022-12-02 00:31:41 +0100

    [Frontend] input component

--- frontend/src/app/components/input/input.component.ts
+++ frontend/src/app/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.scss']
 })
 export class InputComponent implements OnInit {
+  @Input() placeholder: string = '';
+  @Input() type: string = 'text';
+  @Input() value: string = '';
+  @Input() class: string = '';
+  @Output() valueChange = new EventEmitter<string>();
 
   constructor() { }
 
