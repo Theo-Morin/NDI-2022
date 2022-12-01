e60f7fd7dde3922590f824823ca20927373a79f4
drawbu <69208565+drawbu@users.noreply.github.com>
2022-12-01 21:13:31 +0100

    [Frontend] Add disclaimer component

--- frontend/src/app/components/shared.module.ts
+++ frontend/src/app/components/shared.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { ButtonComponent } from './button/button.component';
-
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
 
 
 @NgModule({
   declarations: [
     InputComponent,
-    ButtonComponent
+    ButtonComponent,
+    DisclaimerComponent,
   ],
   exports: [
     InputComponent,
-    ButtonComponent
+    ButtonComponent,
+    DisclaimerComponent,
   ],
   imports: [
     CommonModule
