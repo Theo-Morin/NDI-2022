8ef8bb93f5b288b875dc9e4b76eda98bed30c6f4
Théo MORIN <theo@hetchr.com>
2022-12-01 21:36:22 +0100

    [Frontend] dropdown

--- frontend/src/app/components/shared.module.ts
+++ frontend/src/app/components/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { ButtonComponent } from './button/button.component';
 import { DisclaimerComponent } from './disclaimer/disclaimer.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+
 
 
 @NgModule({
@@ -10,11 +12,13 @@ import { DisclaimerComponent } from './disclaimer/disclaimer.component';
     InputComponent,
     ButtonComponent,
     DisclaimerComponent,
+    DropdownComponent
   ],
   exports: [
     InputComponent,
     ButtonComponent,
     DisclaimerComponent,
+    DropdownComponent
   ],
   imports: [
     CommonModule
