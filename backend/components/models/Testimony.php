6b5d333922d7601f3522ce1264c624e1fd705d70
Sylvain Chiron <sylvain.chiron@etu.u-bordeaux.fr>
2022-12-02 00:41:09 +0100

    [Backend] Testimonies

---
+++ backend/components/models/Testimony.php
@@ -0,0 +1,15 @@
+<?php
+
+class Testimony {
+    private function __construct() {}
+
+    public static function getAll() {
+        $stmt = Database::getInstance()->query('SELECT * FROM testimony');
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public static function create($title, $author, $date, $content) {
+        $stmt = Database::getInstance()->prepare('INSERT INTO testimony(title, author, date, content) VALUES (?, ?, ?, ?)');
+        $stmt->execute([$title, $author, $date, $content]);
+    }
+}
