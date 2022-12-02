086e1436a404c7ffe27aef1f2936050f89931712
Sylvain Chiron <sylvain.chiron@etu.u-bordeaux.fr>
2022-12-02 03:11:49 +0100

    [Backend] Order FAQ contents by pos

--- backend/components/models/FAQ.php
+++ backend/components/models/FAQ.php
@@ -4,7 +4,7 @@ class Faq {
     public $categories = [];
 
     public function __construct() {
-        $stmt = Database::getInstance()->query('SELECT * FROM faq_category');
+        $stmt = Database::getInstance()->query('SELECT * FROM faq_category ORDER BY pos');
         while ($category = $stmt->fetchObject()) {
             $this->categories[$category->id] = $category;
         }
@@ -15,7 +15,7 @@ class Faq {
             $category->questions = [];
         }
 
-        $stmt = Database::getInstance()->query('SELECT * FROM faq_question');
+        $stmt = Database::getInstance()->query('SELECT * FROM faq_question ORDER BY pos');
         $questions = $stmt->fetchAll(PDO::FETCH_OBJ);
         foreach ($questions as $qu) {
             $this->categories[$qu->categoryId]->questions[] = $qu;
@@ -28,7 +28,7 @@ class FaqCategory {
     public $questions;
 
     public function __construct($categoryId) {
-        $stmt = Database::getInstance()->prepare('SELECT * FROM faq_question WHERE categoryId = ?');
+        $stmt = Database::getInstance()->prepare('SELECT * FROM faq_question WHERE categoryId = ? ORDER BY pos');
         $stmt->execute([$categoryId]);
         $this->questions = $stmt->fetchAll(PDO::FETCH_OBJ);
     }
