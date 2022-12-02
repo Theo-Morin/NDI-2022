2e368fa8b63684af993e648fc9a7e3658287ee76
Théo MORIN <theo@hetchr.com>
2022-12-02 05:26:36 +0100

    [Frontend] fix CI

--- frontend/src/app/pages/quiz/quiz-content/quiz-content.component.spec.ts
+++ frontend/src/app/pages/quiz/quiz-content/quiz-content.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { QuizContentComponent } from './quiz-content.component';
 
@@ -8,7 +11,8 @@ describe('QuizContentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ QuizContentComponent ]
+      declarations: [ QuizContentComponent ],
+      imports: [HttpClientModule, RouterTestingModule]
     })
     .compileComponents();
 
