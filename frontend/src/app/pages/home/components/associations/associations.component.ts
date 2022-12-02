6c3949b275ca5c664a8dda5b5728f41a03a57c23
Théo MORIN <theo@hetchr.com>
2022-12-02 05:41:29 +0100

    [Frontend] connection associations

--- frontend/src/app/pages/home/components/associations/associations.component.ts
+++ frontend/src/app/pages/home/components/associations/associations.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OrganizationService } from 'src/app/core/services/organizations.service';
 @Component({
   selector: 'app-associations',
   templateUrl: './associations.component.html',
   styleUrls: ['./associations.component.scss']
 })
 export class AssociationsComponent implements OnInit {
-  constructor() {
+  assoc: any[] = [];
+  constructor(
+    private organizationsService: OrganizationService
+  ) {
   }
 
   ngOnInit(): void {
+    this.organizationsService.get().subscribe((data) => {
+      console.log(data);
+      this.assoc = data;
+    });
   }
 
 }
--- frontend/src/app/pages/home/components/associations/components/association/association.component.ts
+++ frontend/src/app/pages/home/components/associations/components/association/association.component.ts
@@ -1,14 +1,14 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-association-component',
+  selector: 'app-association',
   templateUrl: './association.component.html',
   styleUrls: ['./association.component.scss']
 })
 export class AssociationComponent implements OnInit {
   @Input() name: string;
   @Input() link: string;
-  @Input() image: string;
+  @Input() img: string;
 
   constructor() { }
 
